0585731ed32b7612b3f79dc6ff2e001ad94239f6
Jackson Clark <jclark58@illinois.edu>
2024-12-30 00:22:34 -0600

    Add redis host and port to parameters.

--- deploy_ow_actions/deploy_ow_actions.sh
+++ deploy_ow_actions/deploy_ow_actions.sh
@@ -5,6 +5,11 @@ if [ -z "$WSK_API_HOST" ] || [ -z "$WSK_AUTH_KEY" ]; then
   exit 1
 fi
 
+if [ -z "$REDIS_HSOST" ] || [ -z "$REDIS_PORT" ]; then
+  echo "Error. REDIS_HOST or REDIST_PORT not set."
+  exit 1
+fi
+
 wsk property set --apihost "$WSK_API_HOST" --auth "$WSK_AUTH_KEY"
 
 for dir in actions/*; do
@@ -25,17 +30,17 @@ wsk -i action create seat-service actions/SeatService/function.zip --docker open
   --param WSK_API_HOST "$WSK_API_HOST" \
   --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
 wsk -i action create save-order-info actions/SaveOrderInfo/function.zip --docker openwhisk/python3action --timeout 120000 \
-  --param WSK_API_HOST "$WSK_API_HOST" \
-  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+  --param REDIS_HOST "$REDIS_HOST" \
+  --param REDIS_PORT "$REDIS_PORT"
 wsk -i action create query-for-station-id-by-station-name actions/QueryForStationIdByStationName/function.zip --docker openwhisk/python3action --timeout 120000 \
-  --param WSK_API_HOST "$WSK_API_HOST" \
-  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+  --param REDIS_HOST "$REDIS_HOST" \
+  --param REDIS_PORT "$REDIS_PORT"
 wsk -i action create query-config-entity-by-config-name actions/QueryConfigEntityByConfigName/function.zip --docker openwhisk/python3action --timeout 120000 \
-  --param WSK_API_HOST "$WSK_API_HOST" \
-  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+  --param REDIS_HOST "$REDIS_HOST" \
+  --param REDIS_PORT "$REDIS_PORT"
 wsk -i action create get-plane-type-by-trip-id actions/GetPlaneTypeByTripId/function.zip --docker openwhisk/python3action --timeout 120000 \
-  --param WSK_API_HOST "$WSK_API_HOST" \
-  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+  --param REDIS_HOST "$REDIS_HOST" \
+  --param REDIS_PORT "$REDIS_PORT"
 wsk -i action create get-plane-type-by-plane-type-id actions/GetPlaneTypeByPlaneTypeId/function.zip --docker openwhisk/python3action --timeout 120000 \
   --param WSK_API_HOST "$WSK_API_HOST" \
   --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
@@ -43,20 +48,22 @@ wsk -i action create get-sold-tickets actions/GetSoldTickets/function.zip --dock
   --param WSK_API_HOST "$WSK_API_HOST" \
   --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
 wsk -i action create get-route-by-route-id actions/GetRouteByRouteId/function.zip --docker openwhisk/python3action --timeout 120000 \
-  --param WSK_API_HOST "$WSK_API_HOST" \
-  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+  --param REDIS_HOST "$REDIS_HOST" \
+  --param REDIS_PORT "$REDIS_PORT"
 wsk -i action create get-route-by-trip-id actions/GetRouteByTripId/function.zip --docker openwhisk/python3action --timeout 120000 \
   --param WSK_API_HOST "$WSK_API_HOST" \
-  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY" \
+  --param REDIS_HOST "$REDIS_HOST" \
+  --param REDIS_PORT "$REDIS_PORT"
 wsk -i action create get-price-by-route-id-and-plane-type actions/GetPriceByRouteIdAndPlaneType/function.zip --docker openwhisk/python3action --timeout 120000 \
-  --param WSK_API_HOST "$WSK_API_HOST" \
-  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+  --param REDIS_HOST "$REDIS_HOST" \
+  --param REDIS_PORT "$REDIS_PORT"
 wsk -i action create get-order-by-id actions/GetOrderById/function.zip --docker openwhisk/python3action --timeout 120000 \
-  --param WSK_API_HOST "$WSK_API_HOST" \
-  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+  --param REDIS_HOST "$REDIS_HOST" \
+  --param REDIS_PORT "$REDIS_PORT"
 wsk -i action create drawback actions/Drawback/function.zip --docker openwhisk/python3action --timeout 120000 \
-  --param WSK_API_HOST "$WSK_API_HOST" \
-  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+  --param REDIS_HOST "$REDIS_HOST" \
+  --param REDIS_PORT "$REDIS_PORT"
 wsk -i action create cancel-service actions/CancelService/function.zip --docker openwhisk/python3action --timeout 120000 \
   --param WSK_API_HOST "$WSK_API_HOST" \
   --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
