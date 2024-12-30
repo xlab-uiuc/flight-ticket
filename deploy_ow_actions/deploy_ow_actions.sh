28599fd18a59c91bd66914674f66e4f7986ccd7c
Jackson Clark <jclark58@illinois.edu>
2024-12-29 23:41:16 -0600

    Add parameters to openwhisk actions so it can receive creds

--- deploy_ow_actions/deploy_ow_actions.sh
+++ deploy_ow_actions/deploy_ow_actions.sh
@@ -11,27 +11,52 @@ for dir in actions/*; do
   (
     cd "$dir" || continue
 
-    python3 -m venv virtualenv
-    source virtualenv/bin/activate
-    pip install --no-cache-dir -r requirements.txt
-
+    docker run --rm -v "$PWD:/tmp" openwhisk/python3action bash -c \
+      "cd /tmp && virtualenv virtualenv && source virtualenv/bin/activate && pip install redis"
+    
     zip -r function.zip ./*
-
-    deactivate
   )
 done
 
-wsk -i action create query-for-travel actions/QueryForTravel/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create seat-service actions/SeatService/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create save-order-info actions/SaveOrderInfo/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create query-for-station-id-by-station-name actions/QueryForStationIdByStationName/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create query-config-entity-by-config-name actions/QueryConfigEntityByConfigName/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create get-plane-type-by-trip-id actions/GetPlaneTypeByTripId/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create get-plane-type-by-plane-type-id actions/GetPlaneTypeByPlaneTypeId/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create get-sold-tickets actions/GetSoldTickets/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create get-route-by-route-id actions/GetRouteByRouteId/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create get-route-by-trip-id actions/GetRouteByTripId/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create get-price-by-route-id-and-plane-type actions/GetPriceByRouteIdAndPlaneType/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create get-order-by-id actions/GetOrderById/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create drawback actions/Drawback/function.zip --docker openwhisk/python3action --timeout 120000
-wsk -i action create cancel-service actions/CancelService/function.zip --docker openwhisk/python3action --timeout 120000
+wsk -i action create query-for-travel actions/QueryForTravel/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create seat-service actions/SeatService/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create save-order-info actions/SaveOrderInfo/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create query-for-station-id-by-station-name actions/QueryForStationIdByStationName/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create query-config-entity-by-config-name actions/QueryConfigEntityByConfigName/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create get-plane-type-by-trip-id actions/GetPlaneTypeByTripId/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create get-plane-type-by-plane-type-id actions/GetPlaneTypeByPlaneTypeId/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create get-sold-tickets actions/GetSoldTickets/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create get-route-by-route-id actions/GetRouteByRouteId/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create get-route-by-trip-id actions/GetRouteByTripId/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create get-price-by-route-id-and-plane-type actions/GetPriceByRouteIdAndPlaneType/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create get-order-by-id actions/GetOrderById/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create drawback actions/Drawback/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
+wsk -i action create cancel-service actions/CancelService/function.zip --docker openwhisk/python3action --timeout 120000 \
+  --param WSK_API_HOST "$WSK_API_HOST" \
+  --param WSK_AUTH_KEY "$WSK_AUTH_KEY"
